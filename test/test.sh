11175d8aec22bdc001a01cfd16c7de21123af59e
s163691 <s163691@bitbucket.org>
2018-07-17 16:29:53 +0200

    Added test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-plasmidfinder.py -i /test/test.fsa -o /test/ -mp blastn
+plasmidfinder.py -i /test/test.fsa -o /test/ -mp blastn -x
 file=/test/results_tab.txt
 DIFF=$(diff $file /test/test_results.txt)
 if [ "$DIFF" == "" ] && [ -s $file ] ;

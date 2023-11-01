4fecf9f93c5cd7b456a00ee98ce6c9c14bde4262
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-11-01 16:53:17 +0100

    chk

--- conda/make_conda.py
+++ conda/make_conda.py
@@ -36,7 +36,7 @@ data['about'] = about
 data['extra'] = extra
 
 # Serialize the data to YAML and print it
-yaml = ruamel.yaml.YAML(typ='safe', pure=True)
+yaml = ruamel.yaml.YAML(typ='unsafe', pure=True)
 yaml_str = yaml.dump(data, sys.stdout).replace("\"{{", "{{").replace("}}\"", "}}")
 print(yaml_str)
 

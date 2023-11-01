fed818d95532f2e590f64100331e6f97ad4e4041
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-11-01 16:04:46 +0100

    chk

--- conda/make_conda.py
+++ conda/make_conda.py
@@ -1,33 +1,32 @@
 import ruamel.yaml
 
 # Create an ordered dictionary for each section
-yaml = ruamel.yaml.YAML(typ='safe', pure=True)
-package = yaml.comments.CommentedMap()
+package = ruamel.yaml.comments.CommentedMap()
 package['name'] = 'plasmidfinder'
 package['version'] = '2.1.6'
 
-source = yaml.comments.CommentedMap()
+source = ruamel.yaml.comments.CommentedMap()
 source['url'] = 'https://bitbucket.org/genomicepidemiology/plasmidfinder/get/2.1.6.tar.gz'
 
-build = yaml.comments.CommentedMap()
+build = ruamel.yaml.comments.CommentedMap()
 build['number'] = 1
 build['noarch'] = 'generic'
 
-requirements = yaml.comments.CommentedMap()
+requirements = ruamel.yaml.comments.CommentedMap()
 requirements['host'] = ['python>=3.5', 'wget', 'kma']
 requirements['run'] = ['python>=3.5', 'wget', 'biopython', 'tabulate', 'cgecore', 'blast']
 
-about = yaml.comments.CommentedMap()
+about = ruamel.yaml.comments.CommentedMap()
 about['home'] = 'https://bitbucket.org/genomicepidemiology/plasmidfinder'
 about['summary'] = 'PlasmidFinder service contains one python script plasmidfinder.py which is the script of the latest version of the PlasmidFinder service. The service identifies plasmids in total or partial sequenced isolates of bacteria.'
 about['license'] = 'Apache-2.0'
 
-extra = yaml.comments.CommentedMap()
-identifiers = yaml.comments.CommentedMap()
+extra = ruamel.yaml.comments.CommentedMap()
+identifiers = ruamel.yaml.comments.CommentedMap()
 extra['identifiers'] = identifiers
 
 # Create a dictionary for the entire YAML content
-data = yaml.comments.CommentedMap() 
+data = ruamel.yaml.comments.CommentedMap()
 data['package'] = package
 data['source'] = source
 data['build'] = build
@@ -36,6 +35,7 @@ data['about'] = about
 data['extra'] = extra
 
 # Serialize the data to YAML and print it
+yaml = ruamel.yaml.YAML(typ='safe', pure=True)
 yaml_str = yaml.dump(data, Dumper=yaml.RoundTripDumper).replace("\"{{", "{{").replace("}}\"", "}}")
 print(yaml_str)
 
